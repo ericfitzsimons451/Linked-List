3746ca63874d95957bc662e52f856404e69792cc
Hillary Stewart <hillarystewart4427@gmail.com>
2018-08-26 15:09:06 -0600

    Add class to read button and update css

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var inputWebsite = document.querySelector('#input-website');
 var inputUrl = document.querySelector('#input-url');
 var inputEnter = document.querySelector('#input-enter');
-var cardSection = document.querySelector('.right-side')
+var cardSection = document.querySelector('.right-side');
+var cardReadButton = document.querySelector('.card-read-button');
 
 
 inputEnter.addEventListener('click', createWebsiteLink);
+cardReadButton.addEventListener('click', toggleCardRead);
+
 
 function createWebsiteLink(event) {
   event.preventDefault();
@@ -12,8 +15,14 @@ function createWebsiteLink(event) {
   newCard.innerHTML = `<h2 class='card-website-title'>${inputWebsite.value}</h2>
                         <p class="card-url-input">${inputUrl.value}</p>
                         <footer class="card-footer">
-                        <p class="card-read-button">Read</p>
-                        <p class="card-delete-button">Delete</p></footer>`;
-  cardSection.append(newCard);
+                        <button class="card-read-button" type="button">Read</button>
+                        <button class="card-delete-button" type="button">Delete</button></footer>`;
+  cardSection.prepend(newCard);
 };
+
+function toggleCardRead() {
+	cardReadButton.classList.toggle('read');
+}
+
+
 
